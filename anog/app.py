c0f857bb42190db1a8caa47e019cbe3efbd11525
LorenzoAnogITSAR <lorenzo.anog@itsrizzoli.it>
2025-06-09 17:57:01 +0200

    Esame

--- anog/app.py
+++ anog/app.py
@@ -1,36 +1,29 @@
 from flask import Flask, request, jsonify
+import joblib
 
 app = Flask(__name__)
 
 @app.route('/infer', methods=['POST'])
 def hello():
     data = request.get_json()
-    age = data.get('age')
-    gender = data.get('gender')
-    study_hours_per_day = data.get('study_hours_per_day')
-    social_media_hours = data.get('social_media_hours')
-    netflix_hours = data.get('netflix_hours')
-    part_time_job = data.get('part_time_job')
-    attendance_percentage = data.get('attendance_percentage')
-    sleep_hours = data.get('sleep_hours')
-    diet_quality = data.get('diet_quality')
-    exercise_frequency = data.get('exercise_frequency')
-    parental_education_level = data.get('parental_education_level')
-    internet_quality = data.get('internet_quality')
-    mental_health_rating = data.get('mental_health_rating')
-    extracurricular_participation = data.get('extracurricular_participation')
-    exam_score = data.get('exam_score')
+    features = data.get("features")
 
-    mymodel = joblib.load("C:\Users\LorenzoAnog\Downloads\modello-ml\modello_regressione.joblib")
-    infer_result = mymodel.predict(exam_score)
+    my_model = joblib.load(".../linear_model.joblib")
+    infer_result = my_model.predict(features)
 
     response_data = {
         "result": {
             "value": infer_result
         }
     }
-
+    
     return jsonify(response_data)
+
+@app.route('/infer', methods=['GET'])
+def hello2():
+    # data = request.get_json()
+    # name = data.get('name', 'Stranger')
+    return "<h1>ciao</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- anog/test_app.py
+++ anog/test_app.py
@@ -8,21 +8,10 @@ def client():
         yield client
 
 def test_hello(client):
-    response = client.post("/infer", json={"age": 20, 
-                                           "gender" : "Male",
-                                           "study_hours_per_day": 0.0,
-                                           "social_media_hours" : 5.5,
-                                           "netflix_hours" : 0.0,
-                                           "part_time_job" : "No",
-                                           "attendance_percentage" : 97.3,
-                                           "sleep_hours" : 6.5,
-                                           "diet_quality" : "Fair",
-                                           "exercise_frequency" : 2,
-                                           "parental_education_level" : "High School",
-                                           "internet_quality" : "Good",
-                                           "mental_health_rating" : 8,
-                                           "extracurricular_participation" : "No",
-                                           "exam_score" : 83.3})
+    response = client.post("/infer", 
+                           json={
+                                "features": ["S2000",19,"Female",3.8,1.7,3.8,"No",90.2,5.3,"Good",2,"High School","Average",4,"No",58.1],
+                                })
     assert response.status_code == 200
     data = response.get_json()
-    assert data['result']['value'] == "Hello"
+    assert data == {"message": "Hello Alessandro!"}
